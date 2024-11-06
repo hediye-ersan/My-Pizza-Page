6c57262f8f199bcd2bec951fd31c8ffca476f045
hediye-ersan <hediyesnl9@gmail.com>
2024-11-06 13:57:13 +0300

    header kısmının css clasları düzenlendi.

--- src/App.jsx
+++ src/App.jsx
@@ -89,21 +89,21 @@ function App() {
     <>
       <div className='header'>
         <h1>Teknolojik Yemekler</h1>
-        <div className='gap'>
-          <h3>Anasayfa</h3>
-          <h3>Seçenekler</h3>
-          <h3>Sipariş Oluştur</h3>
+        <div className='form gap'>
+          <h5>Anasayfa</h5>
+          <h5>Seçenekler</h5>
+          <h5>Sipariş Oluştur</h5>
         </div></div>
       <div className='form'>
         <div>
-          <h2 className='gap'>
+          <h2>
             Position Absolute Acı Pizza
           </h2>
           <div className='form-row'>
             <h2>85.50₺</h2>
             <p>4.9</p>
             <p>(200)</p></div>
-          <p className='right'>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
+          <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
 
         </div>
 
