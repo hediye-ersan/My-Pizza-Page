c75997b65ec61b2d58d5fbb8fc9863eb6df77b8a
hediye-ersan <hediyesnl9@gmail.com>
2024-11-08 13:42:00 +0300

    Birden fazla malzeme seçiliyor mu testi.

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -8,6 +8,18 @@ describe('Pizza Siparişi Formu Testi', () => {
             .type('Hediye Ersan')
             .should('have.value', 'Hediye Ersan');
     });
+    it('Birden fazla malzeme seçilmeli', () => {
+
+        cy.get('[data-cy=input-pepperoni]').check();
+        cy.get('[data-cy=input-sosis]').check();
+        cy.get('[data-cy=input-domates]').check();
+
+
+        cy.get('[data-cy=input-pepperoni]').should('be.checked');
+        cy.get('[data-cy=input-sosis]').should('be.checked');
+        cy.get('[data-cy=input-domates]').should('be.checked');
+    });
+
 
     
 });
