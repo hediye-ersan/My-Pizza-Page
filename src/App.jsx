5567305082f5157e7876aa92f646c032ac1a1a98
hediye-ersan <hediyesnl9@gmail.com>
2024-11-08 12:44:45 +0300

    Bileşenler eklendi ve Router oluşturuldu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import workintech from '/workintech.svg'
 import './App.css'
 import { Form, Label, Input, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Router, Switch } from 'react-router-dom';
+
 //Bileşenlerim
 import OrderPizza from './components/OrderPizza';
-
+import Success from './components/Success';
+import Home from './components/Home';
 
 
 
@@ -15,11 +17,23 @@ function App() {
 
   return (
     <>
+      <Router>
+        <Switch>
+
+          <Route path="/orderPizza" />
+          <OrderPizza />
+          <Route path="/Home" />
+          <Home />
+          <Route path="/success" />
+          <Success />
+
+        </Switch>
+      </Router>
       
       <div className='header'>
         <h1>Teknolojik Yemekler</h1>
         <div className='form gap'>
-          <h5>Anasayfa</h5>
+          <h5><Link to="/Home">Anasayfa</Link></h5>
           <h5>Seçenekler</h5>
           <h5>Sipariş Oluştur</h5>
         </div></div>
@@ -35,7 +49,7 @@ function App() {
           <p className='justify'>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p>
 
         </div>
-        <OrderPizza/>
+
       </div>
     </>
   )
