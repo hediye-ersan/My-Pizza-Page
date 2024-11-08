cf1f87c9011c7de7f8110e986cd7dfb65c896f7c
hediye-ersan <hediyesnl9@gmail.com>
2024-11-08 15:37:50 +0300

    Succes.jsx dosyası düzenlendi ve gerekli css kodları eklendi.

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -5,8 +5,13 @@ import { Button } from "reactstrap";
 function Success() {
 
     return(
-        <div>
-        Başarılı!
+        <div className='form succes-center' style={{background:'#CE2829', color:'white'}}>
+        <div className='succes-p'><p>Teknolojik Yemekler</p></div>
+        <div className='succes-container'>
+        <div>TEBRİKLER!</div>
+        <div>SİPARİŞİNİZ</div>
+        <div>ALINDI!</div>
+        </div>
         </div>
     );
 }
