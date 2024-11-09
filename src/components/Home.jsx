c07515e85884e188ed1d56092f09260ebd87a9ab
hediye-ersan <hediyesnl9@gmail.com>
2024-11-09 13:10:58 +0300

    kullanılacak svgler public klasörüne eklendi.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,24 +2,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from "reactstrap";
 
+
+
+
 function Home() {
 
-    return(
+    return (
+        <div>
         <div className='form succes-center bg-img color-white' >
-        <div className='home-p'>
-        <p>Teknolojik Yemekler</p>
-        </div>
-        <div className='home-container'>
-            <div>KOD</div>
-            <div>ACIKTIRIR</div>
-            <div>PİZZA,</div>
-            <div>DOYURUR</div>
-            <Link to ="/orderPizza">
-        <Button className='buttons aciktim-button' data-cy='aciktim'>ACIKTIM!</Button>
-        </Link>
-        </div>
-        
-        
+            <div className='home-p'>
+                <p>Teknolojik Yemekler</p>
+            </div>
+            <div className='home-container'>
+                <div>KOD</div>
+                <div>ACIKTIRIR</div>
+                <div>PİZZA,</div>
+                <div>DOYURUR</div>
+                <Link to="/orderPizza">
+                    <Button className='buttons aciktim-button' data-cy='aciktim'>ACIKTIM!</Button>
+                </Link>
+            </div>
+            </div>
+            <div className='form items'>
+                <div className="item" >
+                    <img src='/icons/1.svg'/>
+                    <h4>YENİ! Kore</h4></div>
+
+                <div className="item">
+                <img src='/icons/2.svg'/>
+                    <h4>Pizza</h4></div>
+
+                <div className="item">
+                <img src='/icons/3.svg'/>
+                    <h4>Burger</h4></div>
+
+                <div className="item">
+                <img src='/icons/4.svg'/>
+                    <h4>Kızartmalar</h4></div>
+
+                <div className="item">
+                <img src='/icons/5.svg'/>
+                    <h4>Fast food</h4></div>
+
+                <div className="item">
+                <img src='/icons/6.svg'/>
+                    <h4>Gazlı içecek</h4></div>
+            </div>
+
         </div>
     );
 }
